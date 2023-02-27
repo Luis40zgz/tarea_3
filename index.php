26e9dd45150c41d53c97ea0b929bb12857dcee83
luis40 <luissg21@gmail.com>
2023-02-27 13:43:47 +0100

    Añado index

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Despliegue</title>
+</head>
+<body>
+    <header>
+        <div class="fondo">
+            <div class="logo">
+                <p class="title-logo"><span class="relleno">*:</span>Retr <span>o</span> Game</p>
+                <div class="img-logo"><img src="./img/mario.png" alt="Super Mario"></div>
+            </div>
+        </div>
+        <nav class="main-menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="./contenido.php">Contenido</a></li>
+                <li><a href="./contacto.php">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>La web del retro gamer</h1>
+        <div class="main-sections">
+            <section class="single-section">
+                <div><img src="./img/space_invaders.jpg" alt="Space Invaders"></div>
+                <article>
+                    <h2>Historia</h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu ex nec diam pellentesque tempus sed in lectus. Donec sit amet lectus auctor nibh condimentum porttitor. Ut dapibus nibh arcu, eget varius felis mollis in. Duis vehicula, nulla quis tincidunt rhoncus, nulla lectus blandit diam, id sagittis felis magna vitae tellus. Cras vel metus at lectus laoreet sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer mi dui, ultrices et arcu nec, molestie ultricies arcu. Nunc fermentum viverra nisl, vel gravida tortor semper ut. Donec ultricies dapibus sem, id tristique ligula posuere dapibus. Aliquam urna nunc, blandit semper venenatis sed, dignissim eu est. Duis suscipit arcu ut tincidunt condimentum. Maecenas vehicula fringilla mauris, vel rhoncus elit hendrerit pharetra. Proin id erat elit. Praesent suscipit gravida lacus, eu pulvinar sapien gravida quis. Duis vestibulum scelerisque auctor. Aenean porta mauris erat, at laoreet metus volutpat id. Nam cursus sem sapien, et accumsan diam gravida eu. Nulla eget lorem at justo venenatis pharetra nec at tortor. Cras at ipsum finibus, ultrices lacus in, scelerisque erat. Morbi fringilla aliquam neque, in volutpat nibh rhoncus sollicitudin. Donec vel mollis dui. Praesent gravida sit amet sem id fermentum. Etiam quis dolor vel lectus tincidunt scelerisque ut eget justo. Etiam ac ultricies nibh. Donec nec lorem imperdiet, viverra eros eu, fringilla nisi. Morbi at nisi risus. Suspendisse auctor est porttitor velit accumsan dictum. Aenean magna leo, congue vitae mattis non, porttitor ac augue. Nunc lectus ligula, accumsan vel dapibus auctor, aliquam eu sem. </p>
+                </article>
+            </section>
+            <section class="single-section">
+                <div><img src="./img/metroid.jpg" alt="Metroid NES"></div>
+                <article>
+                    <h2>Clásicos</h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu ex nec diam pellentesque tempus sed in lectus. Donec sit amet lectus auctor nibh condimentum porttitor. Ut dapibus nibh arcu, eget varius felis mollis in. Duis vehicula, nulla quis tincidunt rhoncus, nulla lectus blandit diam, id sagittis felis magna vitae tellus. Cras vel metus at lectus laoreet sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer mi dui, ultrices et arcu nec, molestie ultricies arcu. Nunc fermentum viverra nisl, vel gravida tortor semper ut. Donec ultricies dapibus sem, id tristique ligula posuere dapibus. Aliquam urna nunc, blandit semper venenatis sed, dignissim eu est. Duis suscipit arcu ut tincidunt condimentum. Maecenas vehicula fringilla mauris, vel rhoncus elit hendrerit pharetra. Proin id erat elit. Praesent suscipit gravida lacus, eu pulvinar sapien gravida quis. Duis vestibulum scelerisque auctor. Aenean porta mauris erat, at laoreet metus volutpat id. Nam cursus sem sapien, et accumsan diam gravida eu. Nulla eget lorem at justo venenatis pharetra nec at tortor. Cras at ipsum finibus, ultrices lacus in, scelerisque erat. Morbi fringilla aliquam neque, in volutpat nibh rhoncus sollicitudin. Donec vel mollis dui. Praesent gravida sit amet sem id fermentum. Etiam quis dolor vel lectus tincidunt scelerisque ut eget justo. Etiam ac ultricies nibh. Donec nec lorem imperdiet, viverra eros eu, fringilla nisi. Morbi at nisi risus. Suspendisse auctor est porttitor velit accumsan dictum. Aenean magna leo, congue vitae mattis non, porttitor ac augue. Nunc lectus ligula, accumsan vel dapibus auctor, aliquam eu sem. </p>
+                </article>
+            </section>
+        </div>
+    </main>
+    <footer>
+        <section>
+            <h2></h2>
+            <p></p>
+        </section>
+        <div class="redes"></div>
+    </footer>
+</body>
+</html>
